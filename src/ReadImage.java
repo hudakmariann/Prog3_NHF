fedb1773902bec06acd9e19f29c4fe00efb18bda
Füzesiné Dr. Hudák Mariann <hudakmariann@gmail.com>
2023-11-23 01:49:36 +0100

    sikerult a binaris resz beolvasasat elkuloniteni a szovegestol

--- src/ReadImage.java
+++ src/ReadImage.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class ReadImage {
 
@@ -7,64 +9,63 @@ public class ReadImage {
 		String inputFile = fileName;
 		String outputFile = "./copy.ppm";
 
+		BufferedReader reader;
+
 		try (InputStream inputStream = new FileInputStream(inputFile);
 				OutputStream outputStream = new FileOutputStream(outputFile);) {
-			int byteRead = -1;
 
-			/*
-			 * while ((byteRead = inputStream.read()) != -1) { outputStream.write(byteRead);
-			 * }
-			 */
+			reader = new BufferedReader(new FileReader(inputFile));
 
-			char[] filetype;
-			filetype = new char[3];
-			for (int i = 0; i < 3; ++i) {
-				filetype[i] = (char) inputStream.read();
-			}
+			String[] fileinfo = new String[3];
+			int i = 0;
+			int linelength = 0;
+			int linecount = 0;
+			int byteRead;
+			String line = reader.readLine();
+			 //while ((byteRead = inputStream.read()) != '\n') {}
+			linelength += line.length();
+			while (Character.isAlphabetic(line.charAt(0)) || Character.isDigit(line.charAt(0))
+					|| line.charAt(0) == '#') {
 
-			if (filetype[0] != 'P' || filetype[1] != '6') {
-				System.out.println("Error: not a valid *.ppm file");
-
-			} else
-				System.out.println("Successful identification as valid *.ppm file");
-
-			// comment kiszurese
-			char c;
-
-			c = (char) inputStream.read();
-
-			if (c == '#') {
-				do {
-				
-					c = (char) inputStream.read();
-				}while (c != '\n');
-			}
-
-			// kep meretenek beolvasasa
-
-			char size[] = new char[10];
-			char depth[] = new char[5];
-
-			size[0] = c; // ha nem volt komment, a kep merete mar itt kezodik, ezert be kell irni a size
-							// tombbe. Ha volt, akkor a \n kerul a size tomb elejere, de azt a sscanf ugysem
-							// fogja szamkent beolvasni, ugyhogy nem baj.
-			System.out.println("Size[0] = " + size[0]);
-			c = (char) inputStream.read();
-			
-			boolean digit = Character.isDigit(c);
-			if (digit) {
-				size[1] = c;
-				System.out.println("Size[1] = " + size[1]);
-				int i = 2;
-				while (c != '\n') {
-
-					c = (char) inputStream.read();
-					size[i] = c;
-					System.out.println("Size[" + i + "] = " + size[i]);
-					i++;
+				if (line.charAt(0) != '#') {// komment kiszurese
+					fileinfo[i] = line;
 				}
-			}
+				System.out.println("fileinfo = " + i + "  " + fileinfo[i]);
+				if (linecount == 0 && line.equals("P6")) {
+					System.out.println("Successful identification as valid *.ppm file. Line : " + line);
 
+				} else if (linecount == 0 && (!(line.equals("P6")))) {
+					System.out.println("Error: not a valid *.ppm file. Line = " + line);
+				}
+				
+				
+						
+				line = reader.readLine();
+				while ((byteRead = inputStream.read()) != '\n') {}
+
+				System.out.println("Line = " + line);
+				if (line.charAt(0) != '#')
+					i++;
+				linecount++;
+
+			}
+			int byteRead2;
+			while ((byteRead2 = inputStream.read()) != -1) {
+                outputStream.write(byteRead2);
+            }
+
+			String[] splited = fileinfo[1].trim().split("\\s+");
+			int Width = Integer.valueOf(splited[0]);
+			int Height = Integer.valueOf(splited[1]);
+			;
+			int Depth = Integer.valueOf(fileinfo[2]);
+
+			System.out.println("Width = " + Width);
+			System.out.println("Height = " + Height);
+			System.out.println("Depth = " + Depth);
+
+
+			reader.close();
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		}
