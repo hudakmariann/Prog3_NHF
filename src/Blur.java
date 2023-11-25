a92327d2c46b8f0d5dc565c34eadc066dd525b09
Füzesiné Dr. Hudák Mariann <hudakmariann@gmail.com>
2023-11-25 19:02:37 +0100

    minden mukodik, mar csak a sallangokat kell kiszedni

---
+++ src/Blur.java
@@ -0,0 +1,107 @@
+
+public class Blur extends HSVBase {
+
+	int filtersize;
+
+	public Blur(double fs) {
+		int filtersize = (int) fs;
+	}
+
+	private void blurImage() {
+
+		double szele = 127;
+		int kozepe;
+		double centerweight;
+
+		if (filtersize < 3) {
+			filtersize = 3;
+			System.out.println("Warning: the requested factor is out of range. The factor is set to 3.\n ");
+		} else if (filtersize > 30) {
+			filtersize = 31;
+			System.out.println("Warning: the requested factor is out of range. The factor is set to 30.\n ");
+		}
+		if (filtersize % 2 == 0)
+			filtersize++;
+
+		kozepe = (filtersize - 1) / 2;
+		centerweight = (filtersize + 1) / 2;
+
+		PixelData[][] filteredIMG = new PixelData[imgParams.Height][imgParams.Width];
+
+		double[][] filter = new double[filtersize][filtersize];
+
+		double filtersum = 0;
+
+		//System.out.print("\nThe blur filter is:\n");
+		for (int i = 0; i < filtersize; i++) {
+			for (int j = 0; j < filtersize; j++) {
+				filter[i][j] = centerweight - ((Math.abs(kozepe - i)) + Math.abs(kozepe - j));
+				if (filter[i][j] < 1)
+					filter[i][j] = 1;
+				// printf("%2.f ", filter[i][j]);
+				filtersum += filter[i][j];
+			}
+			// printf("\n");
+		}
+
+		double r, g, b = 0;
+		for (int y = 0; y < imgParams.Height; y++) {
+			for (int x = 0; x < imgParams.Width; x++) {
+
+				PixelData[][] window = new PixelData[filtersize][filtersize];
+				double windowsum_r = 0;
+				double windowsum_g = 0;
+				double windowsum_b = 0;
+				r = 255;
+				g = 0;
+				b = 0;
+				for (int i = 0; i < filtersize; ++i) {
+					for (int j = 0; j < filtersize; ++j) {
+						if ((y + i > imgParams.Height - 1) || (x + j > imgParams.Width - 1)) {
+							r = szele;
+							g = szele;
+							b = szele;
+						} else {
+							r = imgParams.RGBMatrix[y + i][x + j].r;
+							g = imgParams.RGBMatrix[y + i][x + j].g;
+							b = imgParams.RGBMatrix[y + i][x + j].b;
+						}
+
+						windowsum_r += r * filter[i][j];
+						windowsum_g += g * filter[i][j];
+						windowsum_b += b * filter[i][j];
+					} // end for j
+				} // end for i
+
+				r = windowsum_r / filtersum;
+				g = windowsum_g / filtersum;
+				b = windowsum_b / filtersum;
+				int red = (int) r;
+				int green = (int) g;
+				int blue = (int) b;
+
+				PixelData px = new PixelData(red, green, blue);
+				filteredIMG[y][x] = px;
+
+			} // end for x
+
+		} // end for y
+
+		for (int i = 0; i < imgParams.Height; i++) {
+			for (int j = 0; j < imgParams.Width; j++) {
+				imgParams.RGBMatrix[i][j] = filteredIMG[i][j];
+			} // end for j
+		} // end for i
+		System.out.println("blur kesz.");
+	}
+
+	@Override
+	public void execute(double fsize) {
+
+		this.filtersize = (int) fsize;
+		blurImage();
+		// HSVtoRGB();
+
+	}
+
+}
--- src/GUI.java
+++ src/GUI.java
@@ -6,257 +6,255 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 //import File;
 import java.io.*;
 
-public class GUI extends JFrame{
-	//static final String gapList[] = {"0", "10", "15", "20"};
-    //final static int maxGap = 20;
-    //JComboBox horGapComboBox;
-    //JComboBox verGapComboBox;
-    JButton btnOpen = new JButton("Open Image");
-    JButton btnSave = new JButton("Save");
-    JButton btnSaveAs = new JButton("Save As");
-    JLabel lblFileName = new JLabel("Current Image File");
-    GridLayout experimentLayout = new GridLayout(0,2);
-     
-    public GUI(String name) {
-        super(name);
-        setResizable(false);
-    }
-     
-    /*public void initGaps() {
-        horGapComboBox = new JComboBox(gapList);
-        verGapComboBox = new JComboBox(gapList);
-    }*/
-     
-    public void addComponentsToPane(final Container pane) {
-        //initGaps();
-        final JPanel imageProcControls = new JPanel();
-        imageProcControls.setLayout(experimentLayout);
-        JPanel controls = new JPanel();
-        controls.setLayout(new GridLayout(4,1));
-         
-        //Set up components preferred size
-        /*JButton b = new JButton("Just fake button");
-        Dimension buttonSize = b.getPreferredSize();
-        imageProcControls.setPreferredSize(new Dimension((int)(buttonSize.getWidth() * 2.5)+maxGap,
-                (int)(buttonSize.getHeight() * 3.5)+maxGap * 2));*/
-         
-        //Add buttons to experiment with Grid Layout
-        JButton btnContrast = new JButton("Modify Contrast");
-        imageProcControls.add(btnContrast);
-        JSpinner spContrast = new JSpinner(new SpinnerNumberModel(0,-100,100,1));
-        imageProcControls.add(spContrast);
-       
-        JButton btnBrightness = new JButton("Modify Brightness");
-        imageProcControls.add(btnBrightness);
-        JSpinner spBrightness = new JSpinner(new SpinnerNumberModel(0,-100,100,1));
-        imageProcControls.add(spBrightness);
-        
-        JButton btnSaturation = new JButton("Modify Saturation");
-        imageProcControls.add(btnSaturation);
-        JSpinner spSaturation = new JSpinner(new SpinnerNumberModel(0,-100,100,1));
-        imageProcControls.add(spSaturation);
-        
-        JButton btnBlur = new JButton("Blur image");
-        imageProcControls.add(btnBlur);
-        JSpinner spBlur = new JSpinner(new SpinnerNumberModel(0,0,20,1));
-        imageProcControls.add(spBlur);
-        
-        JButton btnFindEdges = new JButton("Find Edges");
-        imageProcControls.add(btnFindEdges);
-        
+public class GUI extends JFrame {
+	// static final String gapList[] = {"0", "10", "15", "20"};
+	// final static int maxGap = 20;
+	// JComboBox horGapComboBox;
+	// JComboBox verGapComboBox;
+	JButton btnOpen = new JButton("Open Image");
+	JButton btnSave = new JButton("Save");
+	JButton btnSaveAs = new JButton("Save As");
+	JLabel lblFileName = new JLabel("Current Image File");
+	GridLayout experimentLayout = new GridLayout(0, 2);
 
-         
-        //Add controls to set up horizontal and vertical gaps
-        /*controls.add(new Label("Horizontal gap:"));
-        controls.add(new Label("Vertical gap:"));
-        controls.add(new Label(" "));*/
-        controls.add(btnOpen);
-        controls.add(btnSave);
-        controls.add(btnSaveAs);
-        controls.add(lblFileName);
-         
-        //Process the Apply gaps button press
-        
-        btnContrast.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                //Get the horizontal gap value
-            	Integer p = (Integer)spContrast.getValue();
-            	double percent = (double)p;
-            	Contrast contrast = new Contrast(percent);
-                System.out.println("Contrast = " + percent);
-                contrast.execute(percent);
-            }
-        });
-        
-        btnBrightness.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                //Get the horizontal gap value
-            	Integer p = (Integer)spBrightness.getValue();
-            	double percent = (double)p;
-            	Brightness brightness = new Brightness(percent);
-                System.out.println("Brightness = " + percent);
-                brightness.execute(percent);
-               
-            }
-        });
-        
-        btnSaturation.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-            	Integer p = (Integer)spSaturation.getValue();
-            	double percent = (double)p;
-            	Saturation saturation = new Saturation(percent);
-                System.out.println("Saturation = " + percent);
-                saturation.execute(percent);
-            }
-        });
-        
-        btnBlur.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                //Get the horizontal gap value
-                System.out.println("Blur = " + spBlur.getValue());
-            }
-        });
-        
-        btnFindEdges.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                //Get the horizontal gap value
-                System.out.println("FindEdges");
-                FindEdges findedges = new FindEdges();
-                findedges.execute(0);
-            }
-        });
-        
-        btnSaveAs.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                JFileChooser saveAsFileChooser = new JFileChooser(FileSystemView.getFileSystemView());
-                saveAsFileChooser.setAcceptAllFileFilterUsed(false);
-                FileNameExtensionFilter filter = new FileNameExtensionFilter(".ppm", "ppm");
-                saveAsFileChooser.addChoosableFileFilter(filter);
-                //saveAsFileChooser.setFileFilter(filter);
-                
-                // Open the save dialog
-                //saveAsFileChooser.showSaveDialog(null);
-                
-                
-                int r = saveAsFileChooser.showSaveDialog(null);
+	private String inputfilename;
 
-    			// if the user selects a file
-    			if (r == JFileChooser.APPROVE_OPTION)
+	public GUI(String name) {
+		super(name);
+		setResizable(false);
+	}
 
-    			{
-    				// set the label to the path of the selected file
-    				//lblFileName.setText(openFileChooser.getSelectedFile().getAbsolutePath());
-    				//lblFileName.setText(saveAsFileChooser.getSelectedFile().getName());
-    				String fileName = saveAsFileChooser.getSelectedFile().getAbsolutePath();
-    				
-    					fileName+="."+filter.getExtensions()[0];
-    				
-    				WriteImage.writefile(fileName);
-    				
-    				//ReadImage.readfile(fileName);
-    			
+	public void addComponentsToPane(final Container pane) {
+		// initGaps();
+		final JPanel imageProcControls = new JPanel();
+		imageProcControls.setLayout(experimentLayout);
+		JPanel controls = new JPanel();
+		controls.setLayout(new GridLayout(4, 1));
 
-    			}
-    			// if the user cancelled the operation
-    			else
-    				lblFileName.setText("the user cancelled the operation");
-    		}
-                
-                
-            
-        });
-        
-        btnOpen.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                JFileChooser openFileChooser = new JFileChooser(FileSystemView.getFileSystemView());
-                
-                openFileChooser.setAcceptAllFileFilterUsed(false);
-                FileNameExtensionFilter filter = new FileNameExtensionFilter(".ppm", "ppm");
-                openFileChooser.setFileFilter(filter);
-                
-                
-                // Open the save dialog
-                int r = openFileChooser.showOpenDialog(null);
+		// Set up components preferred size
+		/*
+		 * JButton b = new JButton("Just fake button"); Dimension buttonSize =
+		 * b.getPreferredSize(); imageProcControls.setPreferredSize(new
+		 * Dimension((int)(buttonSize.getWidth() * 2.5)+maxGap,
+		 * (int)(buttonSize.getHeight() * 3.5)+maxGap * 2));
+		 */
 
-        			// if the user selects a file
-        			if (r == JFileChooser.APPROVE_OPTION)
+		// Add buttons to experiment with Grid Layout
+		JButton btnContrast = new JButton("Modify Contrast");
+		imageProcControls.add(btnContrast);
+		JSpinner spContrast = new JSpinner(new SpinnerNumberModel(0, -100, 100, 1));
+		imageProcControls.add(spContrast);
 
-        			{
-        				// set the label to the path of the selected file
-        				//lblFileName.setText(openFileChooser.getSelectedFile().getAbsolutePath());
-        				lblFileName.setText(openFileChooser.getSelectedFile().getName());
-        				String fileName = openFileChooser.getSelectedFile().getAbsolutePath();
-        				ReadImage.readfile(fileName);
-	
-        			}
-        			// if the user cancelled the operation
-        			else
-        				lblFileName.setText("the user cancelled the operation");
-        		}
-            
-        });
-        
-       /* applyButton.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                //Get the horizontal gap value
-                String horGap = (String)horGapComboBox.getSelectedItem();
-                //Get the vertical gap value
-                String verGap = (String)verGapComboBox.getSelectedItem();
-                //Set up the horizontal gap value
-                experimentLayout.setHgap(Integer.parseInt(horGap));
-                //Set up the vertical gap value
-                experimentLayout.setVgap(Integer.parseInt(verGap));
-                //Set up the layout of the buttons
-                experimentLayout.layoutContainer(imageProcControls);
-            }
-        });*/
-        pane.add(imageProcControls, BorderLayout.SOUTH);
-        pane.add(new JSeparator(), BorderLayout.CENTER);
-        pane.add(controls, BorderLayout.NORTH);
-    }
-     
-    /**
-     * Create the GUI and show it.  For thread safety,
-     * this method is invoked from the
-     * event dispatch thread.
-     */
-    private static void createAndShowGUI() {
-        //Create and set up the window.
-        GUI frame = new GUI("ImageProcessing");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //Set up the content pane.
-        frame.addComponentsToPane(frame.getContentPane());
-        //Display the window.
-        frame.pack();
-        frame.setVisible(true);
-    }
-     
-    public static void main(String[] args) {
-        /* Use an appropriate Look and Feel */
-        try {
-            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
-            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
-        } catch (UnsupportedLookAndFeelException ex) {
-            ex.printStackTrace();
-        } catch (IllegalAccessException ex) {
-            ex.printStackTrace();
-        } catch (InstantiationException ex) {
-            ex.printStackTrace();
-        } catch (ClassNotFoundException ex) {
-            ex.printStackTrace();
-        }
-        /* Turn off metal's use of bold fonts */
-        UIManager.put("swing.boldMetal", Boolean.FALSE);
-         
-        //Schedule a job for the event dispatch thread:
-        //creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
-            }
-        });
-    }
-	
-	
+		JButton btnBrightness = new JButton("Modify Brightness");
+		imageProcControls.add(btnBrightness);
+		JSpinner spBrightness = new JSpinner(new SpinnerNumberModel(0, -100, 100, 1));
+		imageProcControls.add(spBrightness);
+
+		JButton btnSaturation = new JButton("Modify Saturation");
+		imageProcControls.add(btnSaturation);
+		JSpinner spSaturation = new JSpinner(new SpinnerNumberModel(0, -100, 100, 1));
+		imageProcControls.add(spSaturation);
+
+		JButton btnBlur = new JButton("Blur image");
+		imageProcControls.add(btnBlur);
+		JSpinner spBlur = new JSpinner(new SpinnerNumberModel(0, 0, 20, 1));
+		imageProcControls.add(spBlur);
+
+		JButton btnFindEdges = new JButton("Find Edges");
+		imageProcControls.add(btnFindEdges);
+
+		// Add controls to set up horizontal and vertical gaps
+		/*
+		 * controls.add(new Label("Horizontal gap:")); controls.add(new
+		 * Label("Vertical gap:")); controls.add(new Label(" "));
+		 */
+		controls.add(btnOpen);
+		controls.add(btnSave);
+		controls.add(btnSaveAs);
+		controls.add(lblFileName);
+
+		// Process the Apply gaps button press
+
+		btnContrast.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// Get the horizontal gap value
+				Integer p = (Integer) spContrast.getValue();
+				double percent = (double) p;
+				Contrast contrast = new Contrast(percent);
+				System.out.println("Contrast = " + percent);
+				contrast.execute(percent);
+			}
+		});
+
+		btnBrightness.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// Get the horizontal gap value
+				Integer p = (Integer) spBrightness.getValue();
+				double percent = (double) p;
+				Brightness brightness = new Brightness(percent);
+				System.out.println("Brightness = " + percent);
+				brightness.execute(percent);
+
+			}
+		});
+
+		btnSaturation.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Integer p = (Integer) spSaturation.getValue();
+				double percent = (double) p;
+				Saturation saturation = new Saturation(percent);
+				System.out.println("Saturation = " + percent);
+				saturation.execute(percent);
+			}
+		});
+
+		btnBlur.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Integer fs = (Integer) spBlur.getValue();
+				double fsize = (double) fs;
+				Blur blur = new Blur(fsize);
+				System.out.println("Blur = " + fsize);
+				blur.execute(fsize);
+			}
+		});
+
+		btnFindEdges.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// Get the horizontal gap value
+				System.out.println("FindEdges");
+				FindEdges findedges = new FindEdges();
+				findedges.execute(0);
+			}
+		});
+
+		btnSave.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int input = JOptionPane.showConfirmDialog(null, "Are you sure you want to overwrite the input image?");
+				if (input == 0)
+					WriteImage.writefile(inputfilename);
+			}
+		});
+
+		btnSaveAs.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser saveAsFileChooser = new JFileChooser(FileSystemView.getFileSystemView());
+				saveAsFileChooser.setAcceptAllFileFilterUsed(false);
+				FileNameExtensionFilter filter = new FileNameExtensionFilter(".ppm", "ppm");
+				saveAsFileChooser.addChoosableFileFilter(filter);
+				// saveAsFileChooser.setFileFilter(filter);
+
+				// Open the save dialog
+				// saveAsFileChooser.showSaveDialog(null);
+
+				int r = saveAsFileChooser.showSaveDialog(null);
+
+				// if the user selects a file
+				if (r == JFileChooser.APPROVE_OPTION)
+
+				{
+					// set the label to the path of the selected file
+					// lblFileName.setText(openFileChooser.getSelectedFile().getAbsolutePath());
+					// lblFileName.setText(saveAsFileChooser.getSelectedFile().getName());
+					String fileName = saveAsFileChooser.getSelectedFile().getAbsolutePath();
+
+					fileName += "." + filter.getExtensions()[0];
+
+					WriteImage.writefile(fileName);
+
+					// ReadImage.readfile(fileName);
+
+				}
+				// if the user cancelled the operation
+				else
+					lblFileName.setText("the user cancelled the operation");
+			}
+
+		});
+
+		btnOpen.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser openFileChooser = new JFileChooser(FileSystemView.getFileSystemView());
+
+				openFileChooser.setAcceptAllFileFilterUsed(false);
+				FileNameExtensionFilter filter = new FileNameExtensionFilter(".ppm", "ppm");
+				openFileChooser.setFileFilter(filter);
+
+				// Open the save dialog
+				int r = openFileChooser.showOpenDialog(null);
+
+				// if the user selects a file
+				if (r == JFileChooser.APPROVE_OPTION)
+
+				{
+					// set the label to the path of the selected file
+					// lblFileName.setText(openFileChooser.getSelectedFile().getAbsolutePath());
+					lblFileName.setText(openFileChooser.getSelectedFile().getName());
+					String fileName = openFileChooser.getSelectedFile().getAbsolutePath();
+					ReadImage.readfile(fileName);
+					inputfilename = fileName;
+
+				}
+				// if the user cancelled the operation
+				else
+					lblFileName.setText("the user cancelled the operation");
+			}
+
+		});
+
+		/*
+		 * applyButton.addActionListener(new ActionListener(){ public void
+		 * actionPerformed(ActionEvent e){ //Get the horizontal gap value String horGap
+		 * = (String)horGapComboBox.getSelectedItem(); //Get the vertical gap value
+		 * String verGap = (String)verGapComboBox.getSelectedItem(); //Set up the
+		 * horizontal gap value experimentLayout.setHgap(Integer.parseInt(horGap));
+		 * //Set up the vertical gap value
+		 * experimentLayout.setVgap(Integer.parseInt(verGap)); //Set up the layout of
+		 * the buttons experimentLayout.layoutContainer(imageProcControls); } });
+		 */
+		pane.add(imageProcControls, BorderLayout.SOUTH);
+		pane.add(new JSeparator(), BorderLayout.CENTER);
+		pane.add(controls, BorderLayout.NORTH);
+	}
+
+	/**
+	 * Create the GUI and show it. For thread safety, this method is invoked from
+	 * the event dispatch thread.
+	 */
+	private static void createAndShowGUI() {
+		// Create and set up the window.
+		GUI frame = new GUI("ImageProcessing");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// Set up the content pane.
+		frame.addComponentsToPane(frame.getContentPane());
+		// Display the window.
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		/* Use an appropriate Look and Feel */
+		try {
+			// UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+			UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+		} catch (UnsupportedLookAndFeelException ex) {
+			ex.printStackTrace();
+		} catch (IllegalAccessException ex) {
+			ex.printStackTrace();
+		} catch (InstantiationException ex) {
+			ex.printStackTrace();
+		} catch (ClassNotFoundException ex) {
+			ex.printStackTrace();
+		}
+		/* Turn off metal's use of bold fonts */
+		UIManager.put("swing.boldMetal", Boolean.FALSE);
+
+		// Schedule a job for the event dispatch thread:
+		// creating and showing this application's GUI.
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGUI();
+			}
+		});
+	}
 
 }
